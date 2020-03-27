c98b076e80298af9d7a3357ffbb650542af94159
Serhii <skyiashko@gmail.com>
2020-03-27 08:32:22 +0200

    2020-06-03hw

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-document.querySelector('body').addEventListener('click', clickHandler);
-
-function clickHandler(event) {
-	if (!event.target.hasAttribute('editable')) return;
+document.querySelector('body').addEventListener('click',  cickHandler);
+function  cickHandler(event) {
+	if(!event.target.hasAttribute('editable')) return;
 
 	event.preventDefault();
 
-	const { target: targetElement } = event;
+	const {target:targetElement} = event;
 
 	const type = targetElement.getAttribute('editable');
-
+	console.log('type', type);
 	const inputElement = document.createElement('input');
 	inputElement.setAttribute('type', type);
 
-	targetElement.parentNode.appendChild(inputElement);
-	inputElement.value = targetElement.innerText;
-	targetElement.remove();
-	// targetElement.parentNode.removeChild(targetElement);
+	const save = document.createElement('button');
+	const saveIcon = document.createElement('span');
+	save.classList.add('btn', 'btn-primary', 'btn-xs', 'btn-action');
+	saveIcon.classList.add('glyphicon', 'glyphicon-ok');
 
-	// inputElement.focus();
+	const cancel = document.createElement('button');
+	const cancelIcon = document.createElement('span');
+	cancel.classList.add('btn', 'btn-danger', 'btn-xs', 'btn-action');
+	cancelIcon.classList.add('glyphicon', 'glyphicon-remove');
+
+	targetElement.parentNode.appendChild(inputElement).after(cancel);
+	inputElement.after(save);
+	save.appendChild(saveIcon);
+	cancel.appendChild(cancelIcon);
+	inputElement.value = targetElement.innerText;
+
+	targetElement.remove();
 	inputElement.select();
 
 	inputElement.addEventListener('keyup', function(event) {
 		switch (event.which) {
-			case 13: // save
+			case 13: //save
 				inputElement.parentNode.appendChild(targetElement);
-				// targetElement.innerText = this.value;
-				targetElement.innerText = inputElement.value;
+				targetElement.innerText = this.value;
 				inputElement.remove();
 				break;
-			case 27: // cancel
+			case 27://cancel
 				inputElement.parentNode.appendChild(targetElement);
 				inputElement.remove();
 				break;
 		}
 	});
-}
 
+	save.addEventListener('click', function() {
+		inputElement.parentNode.appendChild(targetElement);
+		targetElement.innerText = inputElement.value;
+		inputElement.remove();
+		this.remove();
+		cancel.remove();
+	});
+
+	cancel.addEventListener('click', function() {
+		inputElement.parentNode.appendChild(targetElement);
+		inputElement.remove();
+		this.remove()
+		save.remove();
+	});
+
+}
